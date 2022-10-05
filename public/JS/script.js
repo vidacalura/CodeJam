e4c9cb42353ea1e494bcd578043396c8517bcd27
vidacalura <lmguedes007@gmail.com>
2022-10-05 18:39:14 -0300

    Finalizada a conexão com a API do CodeWars

--- public/JS/script.js
+++ public/JS/script.js
@@ -5,5 +5,33 @@ const nomeCompetidoresText = document.querySelectorAll(".ranking-nome");
 const pontosCompetidoresText = document.querySelectorAll(".ranking-pontos");
 
 
-
 /* Registro */
+
+socket.on("participantesRes", (data) => {
+    const participantesOrdenados = data;
+
+    for (let i = 0; i < 3; i++){
+        nomeCompetidoresText[i].textContent = participantesOrdenados[participantesOrdenados.length - (i + 1)].username;
+        pontosCompetidoresText[i].textContent = participantesOrdenados[participantesOrdenados.length - (i + 1)].honor;
+    }
+
+    // Usuário
+    if (localStorage.getItem("username")){
+        const usuarioRankingDiv = document.getElementById("voce-lugar");
+        const usuarioRankingNumTxt = document.getElementById("voce-ranking");
+        usuarioRankingDiv.classList.add("ranking");
+
+        const l = participantesOrdenados.length;
+        for (let i = 0; i < l; i++){
+            if (participantesOrdenados[i].username == localStorage.getItem("username")){
+                usuarioRankingNumTxt.textContent = l - i;
+                nomeCompetidoresText[3].textContent = localStorage.getItem("username");
+                pontosCompetidoresText[3].textContent = participantesOrdenados[i].honor;
+
+                break;
+            }
+        }
+
+    }
+
+});
--- server.js
+++ server.js
@@ -8,37 +8,74 @@ const server = app.listen(port);
 let io = socket(server);
 
 app.use(express.json());
+app.use(express.static("./public/"));
 
-app.get("/", () => {
-    // Express section
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: __dirname });
+});
 
-    
-    const sampleData = ["GuedeX", "matheusRodrigues", "awesomead", "Bodiapa", "RealSup"];
-    let participantes = [];
+
+io.on("connection", async (socket) => {
+
+    const sampleData = ["GuedeX", "matheusRodrigues", "awesomead"];
 
     // Pegar dados (nome) do banco
     const nomes = sampleData;
 
     // Puxar dados da API do codewars
+    const participantes = await receberDadosAPI(nomes);
+
+    const participantesOrdenados = organizarParticipantes(participantes);
+
+    io.sockets.emit("participantesRes", participantesOrdenados);
+
+});
+
+
+async function receberDadosAPI(nomes){
+    let participantes = [];
+
     for (let i = 0; i < nomes.length; i++){
         const URL = "https://www.codewars.com/api/v1/users/" + nomes[i];
 
-        fetch(URL)
-        .then((res) => res.json())
-        .then((data) => {
-            participantes.push(data);
-
-            console.log(participantes);
-        });
-
+        const APIData = await getAPIData(URL);
+        participantes.push(APIData);
     }
 
-    io.sockets.emit("participantesRes", participantes);
+    return participantes;
+}
 
-});
+async function getAPIData(url){
 
-io.on("connection", (socket) => {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    return data;
+    
+} 
+
+function organizarParticipantes(participantes){
+
+    let arrOrdenada = [ participantes[0] ];
+
+    for (let i = 1; i < participantes.length; i++){
+        for (let j = 0; j < arrOrdenada.length; j++){
+            if (participantes[i].honor <= arrOrdenada[j].honor){
+                arrOrdenada.splice(j, 0, participantes[i]);
+                break;
+            }
+        }
+        if (!arrOrdenada.includes(participantes[i])){
+            arrOrdenada.push(participantes[i]);
+        }
+    }
+
+    return arrOrdenada;
+
+}
+
+async function receberDadosBD(){
 
 
 
-});
+}
